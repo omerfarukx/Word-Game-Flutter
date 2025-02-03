f1d1f40f758ed8383fada6a8b7439bd92cffe8f0
omerfarukx <adanadunyakargo6@hotmail.com>
2025-02-03 21:01:35 +0300

    update firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
